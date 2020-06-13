c61d2ee14eece50f0f1c32c8bd5343438364e92d
Kevin Leung <kkleung2@uci.edu>
2020-06-13 02:37:04 -0700

    debug environment variable

--- flask/client.py
+++ flask/client.py
@@ -4,6 +4,9 @@ import os
 
 def hypixelBazaarClient():
     load_dotenv()
+
+    print(os.getenv("API_KEY"), flush=True)
+
     response = requests.get("https://api.hypixel.net/skyblock/bazaar?key={}".format(os.getenv("API_KEY")))
 
     if (response.status_code != 200):
