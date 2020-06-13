118baed36a89d92c43678482ea0201251ca19d7b
Kevin Leung <kkleung2@uci.edu>
2020-06-13 02:15:19 -0700

    added single product endpoint

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/single/<productId>', methods=['GET'])
+def getSingleProduct(productId):
+    return(jsonify({"productId": productId}))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
